3a42a5ce4f42ee19adf9f75cc12c7e202ab53632
Johana Wu Nie <johana.wu@ucr.ac.cr>
2024-09-15 08:36:45 -0600

    Update App, index and some configurations for new packages

--- infinity_fashion/src/App.tsx
+++ infinity_fashion/src/App.tsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import BestSeller from './components/bestSeller/BestSeller';
+import NewArrival from './components/newArrival/NewArrival';
+import Top from './components/pages/Top';
+import Bottom from './components/pages/Bottom';
+import Outwear from './components/pages/Outwear';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top" element={<Top />} />
+          <Route path="/bottom" element={<Bottom />} />
+          <Route path="/outwear" element={<Outwear />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
