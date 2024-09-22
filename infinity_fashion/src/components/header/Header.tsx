7e9d8aebcb7c361e413b8aa702b3bff76c4ed8ce
Johana Wu Nie <johana.wu@ucr.ac.cr>
2024-09-22 09:29:03 -0600

    add header

--- infinity_fashion/src/components/header/Header.tsx
+++ infinity_fashion/src/components/header/Header.tsx
@@ -1,64 +1,60 @@
 import React from 'react';
-import "./header.css";
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-import img1 from '../../assets/Home/logo.png'; 
+import img1 from '../../assets/Home/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import './header.css';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid className="d-flex justify-content-center">
-          <Navbar.Brand href="#">
-            <img
-              src={img1}
-              alt="Logo"
-              width="200" 
-              height="100" 
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+    <header>
+      <Container className="logo-container">
+        <img
+          src={img1}
+          className="logo"
+          alt="Infinity Fashion Logo"
+        />
+      </Container>
 
-         <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid className="d-flex justify-content-center">
-         <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-           <Nav className="ms-auto">
-            <Nav.Link href="#signIn">Sign In</Nav.Link>
-            <Nav.Link href="#shoppingBag">Shopping Bag</Nav.Link>
-          </Nav>
-             
-        </Container>
-      </Navbar>
-
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="navbar-custom">
         <Container fluid>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav className="me-auto my-2 my-lg-0 mx-auto nav-links" style={{ maxHeight: '100px' }} navbarScroll>
+
+            <Nav className="nav-links">
               <Link className="nav-link" to='/'>HOME</Link>
               <Link className="nav-link" to='/top'>TOPS</Link>
               <Link className="nav-link" to='/bottom'>BOTTOMS</Link>
               <Link className="nav-link" to='/outwear'>OUTWEARS</Link>
             </Nav>
+
+            <div className="nav-right">
+              <Form className="search-form">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="search-input"
+                  aria-label="Search"
+                />
+                <Button variant="outline-dark" className="search-button">Search</Button>
+              </Form>
+              <Nav className="auth-links">
+                <Link className="nav-link" to='/signIn'>Sign In</Link>
+                <Link className="nav-link" to='/shoppingBag'>
+                  <FontAwesomeIcon icon={faShoppingBag} size="xl" />
+                </Link>
+              </Nav>
+            </div>
+
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
-    </div>
+    </header>
   );
 };
 
