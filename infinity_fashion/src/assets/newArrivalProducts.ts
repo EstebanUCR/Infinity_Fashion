d38da75fa7183f5dfe800337ae49f86901847adb
Johana Wu Nie <johana.wu@ucr.ac.cr>
2024-09-25 21:50:18 -0600

    Add New Arrival products

---
+++ infinity_fashion/src/assets/newArrivalProducts.ts
@@ -0,0 +1,84 @@
+import p7_img from "./productImages/corduroy-loose-fit-crop-jacket.png";
+import p8_img from "./productImages/oversized-blazer.png";
+import p9_img from "./productImages/open-front-eyelet-knit-cardigan.png";
+import p10_img from "./productImages/starfish-stud-earrings.png";
+import p11_img from "./productImages/carnelian-stone-heart-necklace.png";
+import p12_img from "./productImages/sparkle-clover-bracelet.png";
+import p13_img from "./productImages/platform-lace-up-oxford-shoes.png";
+import p14_img from "./productImages/casual-shoes-with-buckle-closure.png";
+import p15_img from "./productImages/black-loafers.png";
+
+let newArrivals_products = [
+   {
+    id: 9,
+    category: "outerwear",
+    image: p9_img,
+    name: 'Open-Front Eyelet-Knit Cardigan',
+    price: '$17.99',
+    isExclusive: false,
+  },
+  {
+    id: 10,
+    category: "accessories",
+    image: p10_img,
+    name: 'Starfish Stud Earrings',
+    price: '$24.00',
+    isExclusive: false,
+  },
+  {
+    id: 11,
+    category: "accessories",
+    image: p11_img,
+    name: 'Carnelian Stone Heart Necklace',
+    price: '$21.60',
+    oldPrice: '$27.00',
+    discount: '20% OFF',
+    isExclusive: false,
+  },
+  {
+    id: 12,
+    category: "accessories",
+    image: p12_img,
+    name: 'Sparkle Clover Bracelet',
+    price: '$26.00',
+    isExclusive: true,
+  },
+  {
+    id: 13,
+    category: "shoes",
+    image: p13_img,
+    name: 'Platform Lace-Up Oxford Shoes',
+    price: '$29.49',
+    isExclusive: true,
+  },
+  {
+    id: 14,
+    category: "shoes",
+    image: p14_img,
+    name: 'Casual Shoes with Buckle Closure',
+    price: '$17.49',
+    isExclusive: true,
+  },
+  {
+    id: 15,
+    category: "shoes",
+    image: p15_img,
+    name: 'Black Loafers',
+    price: '$25.99',
+    isExclusive: false,
+  },
+  {
+    id: 7,
+    category: "outerwear",
+    image: p7_img,
+    name: 'Corduroy Loose-Fit Crop Jacket',
+    price: '$17.49',
+    oldPrice: '$24.99',
+    discount: '30% OFF',
+    isExclusive: true,
+  },
+
+ 
+];
+
+export default newArrivals_products;
