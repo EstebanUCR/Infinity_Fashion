c576e51175f90b312addaf2a8dfa1fa789a4774e
ESTEBAN.IGLESIAS@ucr.ac.cr <ESTEBAN.IGLESIAS@ucr.ac.cr>
2024-09-30 18:53:26 -0600

    And link to the imagen and styles for product

--- infinity_fashion/src/components/productCard/Product.tsx
+++ infinity_fashion/src/components/productCard/Product.tsx
@@ -11,7 +11,7 @@ export default function ProductCard ({product, addToCart} : ProductProps)  {
   return (
     <div className="product-card">
       <div className="image-container">
-        <img src={product.image} alt={product.name} />
+        <Link to={`/product/${product.id}`}> <img src={product.image} alt={product.name} /> </Link>
       </div>
       <div className="product-details">
         <h3>{product.name}</h3>
