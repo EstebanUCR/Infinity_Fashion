ce21d245a81023bed9634b1826ea6a4ec22ef583
Johana Wu Nie <johana.wu@ucr.ac.cr>
2024-10-31 18:49:38 -0600

    add footer and header in signin/signup

--- infinity_fashion/src/components/pages/SignIn.tsx
+++ infinity_fashion/src/components/pages/SignIn.tsx
@@ -10,6 +10,9 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from '../../firebaseConfig';
 import { useUserContext } from '../Context/userContext';
 import { useNavigate } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import img1 from '../../assets/Home/logoWithOutBackground.png';
+import Footer from '../footer/Footer';
 
 const SignIn = () => {
 
@@ -25,15 +28,15 @@ const SignIn = () => {
   const [showPasswordSignIn, setShowPasswordSignIn] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
   const { loginUser } = useUserContext();
-  
-  const handleGoogleAuth = async () => {
-  try {
-    const result = await signInWithPopup(auth, googleProvider);
-    const { displayName, email } = result.user;
 
-    if (!isRightPanelActive) {
-      // Modo Sign In
-       const response = await fetch('http://localhost:3000/signin', {
+  const handleGoogleAuth = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const { displayName, email } = result.user;
+
+      if (!isRightPanelActive) {
+        // Modo Sign In
+        const response = await fetch('http://localhost:3000/signin', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email }), // Solo enviamos el email en el caso de Google
@@ -50,31 +53,31 @@ const SignIn = () => {
         } else {
           alert("Este usuario no está registrado. Por favor, regístrese primero.");
         }
-    } else {
-      // Modo Sign Up
-      const response = await fetch('http://localhost:3000/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: displayName, email, password: '' }),
-      });
-
-      const data = await response.json();
-      if (response.ok) {
-        alert(data.message); // Registro exitoso
-        localStorage.setItem('token', data.token); // Guardamos el token en el localStorage
-        loginUser({ name: displayName || '', email: email || '' });
-        localStorage.setItem('name', displayName || '');
-        localStorage.setItem('email', email || '');
-        navigate('/');
       } else {
-        alert(data.message); // Error en el registro (usuario ya registrado)
+        // Modo Sign Up
+        const response = await fetch('http://localhost:3000/signup', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name: displayName, email, password: '' }),
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+          alert(data.message); // Registro exitoso
+          localStorage.setItem('token', data.token); // Guardamos el token en el localStorage
+          loginUser({ name: displayName || '', email: email || '' });
+          localStorage.setItem('name', displayName || '');
+          localStorage.setItem('email', email || '');
+          navigate('/');
+        } else {
+          alert(data.message); // Error en el registro (usuario ya registrado)
+        }
       }
+    } catch (error) {
+      console.error("Error con la autenticación de Google:", error);
+      alert("Error con la autenticación de Google. Por favor, inténtelo de nuevo.");
     }
-  } catch (error) {
-    console.error("Error con la autenticación de Google:", error);
-    alert("Error con la autenticación de Google. Por favor, inténtelo de nuevo.");
-  }
-};
+  };
   const togglePasswordVisibilitySignUp = () => {
     setShowPasswordSignUp(!showPasswordSignUp);
   };
@@ -88,12 +91,12 @@ const SignIn = () => {
   };
 
 
-  const handleSignUpClick = () => {
-    setIsRightPanelActive(true);
-  };
-
   const handleSignInClick = () => {
     setIsRightPanelActive(false);
+  };
+
+  const handleSignUpClick = () => {
+    setIsRightPanelActive(true);
   };
 
   const handleSignInSubmit = async (e: React.FormEvent) => {
@@ -111,13 +114,13 @@ const SignIn = () => {
         });
         const data = await response.json();
 
-         if (response.ok) {
+        if (response.ok) {
           localStorage.setItem('token', data.token);
           loginUser({ name: data.userName, email: user.email }); // Set user in context
           localStorage.setItem('name', data.userName);
           localStorage.setItem('email', user.email);
           navigate('/');
-        } 
+        }
         alert(data.message);
       } catch (error) {
         console.error('Error:', error);
@@ -170,135 +173,146 @@ const SignIn = () => {
   };
 
   return (
-    <div className={`${styles.container} ${isRightPanelActive ? styles.rightPanelActive : ''}`} id="container">
-      {/* Contenedor de Sign Up */}
-      <div className={`${styles.formContainer} ${styles.signUpContainer}`}>
-        <form onSubmit={handleSignUpSubmit}>
-          <h1>Create Account</h1>
-          <div className={styles.socialContainer}>
-            <a href="#" className={styles.social} onClick={handleGoogleAuth}>
-              <img src={googleLogo} alt="Google" className={styles.googleLogo} />
-            </a>
-          </div>
-          <span>or use your email for registration</span>
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={user.name}
-            onChange={handleInputChange}
-            className={signUpErrors.name && isRightPanelActive ? `${styles.inputError}` : ''}
-          />
-          {renderErrors(signUpErrors.name)}
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={user.email}
-            onChange={handleInputChange}
-            className={signUpErrors.email && isRightPanelActive ? `${styles.inputError}` : ''}
-          />
-          {renderErrors(signUpErrors.email)}
-          <div className={styles.passwordContainer}>
+    <>
+      <Container fluid className="logo-container">
+        <img
+          src={img1}
+          className="logo"
+          alt="Infinity Fashion Logo"
+        />
+      </Container>
+
+      <div className={`${styles.container} ${isRightPanelActive ? styles.rightPanelActive : ''}`} id="container">
+        {/* Contenedor de Sign Up */}
+        <div className={`${styles.formContainer} ${styles.signUpContainer}`}>
+          <form onSubmit={handleSignUpSubmit}>
+            <h1>Create Account</h1>
+            <div className={styles.socialContainer}>
+              <a href="#" className={styles.social} onClick={handleGoogleAuth}>
+                <img src={googleLogo} alt="Google" className={styles.googleLogo} />
+              </a>
+            </div>
+            <span>or use your email for registration</span>
             <input
-              type={showPasswordSignUp ? "text" : "password"}
-              placeholder="Password"
-              name="password"
-              value={user.password}
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={user.name}
               onChange={handleInputChange}
-              className={signUpErrors.password && isRightPanelActive ? `${styles.inputError}` : ''}
+              className={signUpErrors.name && isRightPanelActive ? `${styles.inputError}` : ''}
             />
-            <span onClick={togglePasswordVisibilitySignUp} className={styles.eyeIcon}>
-              <img
-                src={showPasswordSignUp ? eyeOpenIcon : eyeClosedIcon}
-                alt={showPasswordSignUp ? "Hide password" : "Show password"}
-                className={styles.eyeImage}
-              />
-            </span>
-          </div>
-          {renderErrors(signUpErrors.password)}
-          <div className={styles.passwordContainer}>
+            {renderErrors(signUpErrors.name)}
             <input
-              type={showConfirmPassword ? "text" : "password"}
-              placeholder="Confirm Password"
-              name="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className={signUpErrors.confirmPassword && isRightPanelActive ? `${styles.inputError}` : ''}
-            />
-            <span onClick={toggleVisibilityConfirmPassword} className={styles.eyeIcon}>
-              <img
-                src={showConfirmPassword ? eyeOpenIcon : eyeClosedIcon}
-                alt={showConfirmPassword ? "Hide password" : "Show password"}
-                className={styles.eyeImage}
-              />
-            </span>
-          </div>
-          {renderErrors(signUpErrors.confirmPassword)}
-          <button type="submit">Sign Up</button>
-        </form>
-      </div>
-
-      {/* Contenedor de Sign In */}
-      <div className={`${styles.formContainer} ${styles.signInContainer}`}>
-        <form onSubmit={handleSignInSubmit}>
-          <h1>Sign in</h1>
-          <div className={styles.socialContainer}>
-            <a href="#" className={styles.social} onClick={handleGoogleAuth}>
-              <img src={googleLogo} alt="Google" className={styles.googleLogo} />
-            </a>
-          </div>
-          <span>or use your account</span>
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={user.email}
-            onChange={handleInputChange}
-            className={signInErrors.email && !isRightPanelActive ? `${styles.inputError}` : ''}
-          />
-          {renderErrors(signInErrors.email)}
-
-          <div className={styles.passwordContainer}>
-            <input
-              type={showPasswordSignIn ? "text" : "password"}
-              placeholder="Password"
-              name="password"
-              value={user.password}
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={user.email}
               onChange={handleInputChange}
-              className={signInErrors.password && !isRightPanelActive ? `${styles.inputError}` : ''}
+              className={signUpErrors.email && isRightPanelActive ? `${styles.inputError}` : ''}
             />
-            <span onClick={togglePasswordVisibilitySignIn} className={styles.eyeIcon}>
-              <img
-                src={showPasswordSignIn ? eyeOpenIcon : eyeClosedIcon}
-                alt={showPasswordSignIn ? "Hide password" : "Show password"}
-                className={styles.eyeImage}
+            {renderErrors(signUpErrors.email)}
+            <div className={styles.passwordContainer}>
+              <input
+                type={showPasswordSignUp ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                value={user.password}
+                onChange={handleInputChange}
+                className={signUpErrors.password && isRightPanelActive ? `${styles.inputError}` : ''}
               />
-            </span>
-          </div>
-          {renderErrors(signInErrors.password)}
-          <button type="submit">Sign In</button>
-        </form>
-      </div>
+              <span onClick={togglePasswordVisibilitySignUp} className={styles.eyeIcon}>
+                <img
+                  src={showPasswordSignUp ? eyeOpenIcon : eyeClosedIcon}
+                  alt={showPasswordSignUp ? "Hide password" : "Show password"}
+                  className={styles.eyeImage}
+                />
+              </span>
+            </div>
+            {renderErrors(signUpErrors.password)}
+            <div className={styles.passwordContainer}>
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className={signUpErrors.confirmPassword && isRightPanelActive ? `${styles.inputError}` : ''}
+              />
+              <span onClick={toggleVisibilityConfirmPassword} className={styles.eyeIcon}>
+                <img
+                  src={showConfirmPassword ? eyeOpenIcon : eyeClosedIcon}
+                  alt={showConfirmPassword ? "Hide password" : "Show password"}
+                  className={styles.eyeImage}
+                />
+              </span>
+            </div>
+            {renderErrors(signUpErrors.confirmPassword)}
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
 
-      {/* Overlay */}
-      <div className={styles.overlayContainer}>
-        <div className={styles.overlay}>
-          <div className={`${styles.overlayPanel} ${styles.overlayLeft}`}>
-            <img src={infinityLogo} alt="Infinity" className={styles.companyLogo} />
-            <h1>Glad You're Back!</h1>
-            <h2>Please enter your login details to continue your journey</h2>
-            <button className={styles.ghost} onClick={handleSignInClick}>Sign In</button>
-          </div>
-          <div className={`${styles.overlayPanel} ${styles.overlayRight}`}>
-            <img src={infinityLogo} alt="Infinity" className={styles.companyLogo} />
-            <h1>Welcome Aboard!</h1>
-            <h2>Enter your personal details and start your journey with us</h2>
-            <button className={styles.ghost} onClick={handleSignUpClick}>Sign Up</button>
+        {/* Contenedor de Sign In */}
+           <div className={`${styles.formContainer} ${styles.signInContainer}`}>
+          <form onSubmit={handleSignInSubmit}>
+            <h1>Sign in</h1>
+            <div className={styles.socialContainer}>
+              <a href="#" className={styles.social} onClick={handleGoogleAuth}>
+                <img src={googleLogo} alt="Google" className={styles.googleLogo} />
+              </a>
+            </div>
+            <span>or use your account</span>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={user.email}
+              onChange={handleInputChange}
+              className={signInErrors.email && !isRightPanelActive ? `${styles.inputError}` : ''}
+            />
+            {renderErrors(signInErrors.email)}
+
+            <div className={styles.passwordContainer}>
+              <input
+                type={showPasswordSignIn ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                value={user.password}
+                onChange={handleInputChange}
+                className={signInErrors.password && !isRightPanelActive ? `${styles.inputError}` : ''}
+              />
+              <span onClick={togglePasswordVisibilitySignIn} className={styles.eyeIcon}>
+                <img
+                  src={showPasswordSignIn ? eyeOpenIcon : eyeClosedIcon}
+                  alt={showPasswordSignIn ? "Hide password" : "Show password"}
+                  className={styles.eyeImage}
+                />
+              </span>
+            </div>
+            {renderErrors(signInErrors.password)}
+            <button type="submit">Sign In</button>
+          </form>
+        </div>
+
+        {/* Overlay */}
+        <div className={styles.overlayContainer}>
+          <div className={styles.overlay}>
+            <div className={`${styles.overlayPanel} ${styles.overlayLeft}`}>
+              <img src={infinityLogo} alt="Infinity" className={styles.companyLogo} />
+              <h1>Glad You're Back!</h1>
+              <h2>Please enter your login details to continue your journey</h2>
+              <button className={styles.ghost} onClick={handleSignInClick}>Sign In</button>
+            </div>
+            <div className={`${styles.overlayPanel} ${styles.overlayRight}`}>
+              <img src={infinityLogo} alt="Infinity" className={styles.companyLogo} />
+              <h1>Welcome Aboard!</h1>
+              <h2>Enter your personal details and start your journey with us</h2>
+              <button className={styles.ghost} onClick={handleSignUpClick}>Sign Up</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
