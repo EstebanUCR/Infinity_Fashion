8869119ed6e754cdd8a6e05ec4d2af912b2dadd8
Johana Wu Nie <johana.wu@ucr.ac.cr>
2024-11-16 20:15:22 -0600

    Add updates for userValidation and profile to use the database

--- infinity_fashion/src/types/types.ts
+++ infinity_fashion/src/types/types.ts
@@ -1,23 +1,27 @@
 export type Product = {
-    id: number
-    category: string
-    image: string[]
-    name: string
-    price: number
-    oldPrice?: string
-    discount?: string
-    isExclusive: boolean
-    description?: string
-  }
+  id: number
+  category: string
+  image: string[]
+  name: string
+  price: number
+  oldPrice?: string
+  discount?: string
+  isExclusive: boolean
+  description?: string
+}
 
-  export type userData = {
-    name: string
-    email: string
-    password: string | null
-  }
-  
-  export type CartItem = Product & {
-    quantity: number
-  }
+export type userData = {
+  name: string;
+  email: string;
+  phone?: string | null;
+  password: string | null;
+  shipping_address?: string | null;
+  billing_address?: string | null;
+  profile_picture?: string | null;
+}
 
-  export type ProductID = Product['id']
+export type CartItem = Product & {
+  quantity: number
+}
+
+export type ProductID = Product['id']
