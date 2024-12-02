20395eef4cfe1a206b74d5ff7101906b63d66aab
Esteban Iglesias Vargas <Esteban.Iglesias@ucr.ac.cr>
2024-12-01 21:45:36 -0600

    Problem related to article addressing is fixed

--- infinity_fashion/src/components/productCard/Product.tsx
+++ infinity_fashion/src/components/productCard/Product.tsx
@@ -10,7 +10,7 @@ type ProductProps = {
   // addToCart: (item: databaseProduct) => void
 }
 
-export default function ProductCard ({product} : ProductProps)  {
+export default function ProductCard({ product }: ProductProps) {
 
   const [images, setImages] = useState<productImage[]>([])
 
@@ -24,54 +24,52 @@ export default function ProductCard ({product} : ProductProps)  {
     try {
       const response: productImage[] = await getProductImages(product.id);
       setImages(response)
-      
+
       console.log(response)
 
     } catch (err) {
       console.error('Error fetching product images', err);
-    } 
+    }
   };
 
   return (
     <div className="product-card">
       <div className="image-container">
-        <Link to={`/product/${product.id}`}>
-          {images?.length > 0 ? <img src={images[0].image_data} alt={product.name} /> : null }
+        <Link to={`/product/${product.id}`} state={{ product: product, images: images }}>
+          {images?.length > 0 ? <img src={images[0].image_data} alt={product.name} /> : null}
         </Link>
-        </div>
+      </div>
       <div className="product-details">
         <h3>{product.name}</h3>
         <div className='product-info'>
           {
-            product.discount ? 
-            <div>
-              <span className='price-discount'>
-              ${(product.price - product.price * product.discount).toFixed(2)}
-              </span>{' '}
-              <span className="old-price">${product.price.toFixed(2)}</span>{' '}
-              <span className="discount">{product.discount * 100}% OFF</span>
-            </div>
-            :
-            <div>
-              <span className='price-regular'>
-              ${product.price.toFixed(2)}
-              </span>{' '}
-            </div>
+            product.discount ?
+              <div>
+                <span className='price-discount'>
+                  ${(product.price - product.price * product.discount).toFixed(2)}
+                </span>{' '}
+                <span className="old-price">${product.price.toFixed(2)}</span>{' '}
+                <span className="discount">{product.discount * 100}% OFF</span>
+              </div>
+              :
+              <div>
+                <span className='price-regular'>
+                  ${product.price.toFixed(2)}
+                </span>{' '}
+              </div>
           }
         </div>
       </div>
       <div className='d-flex justify-content-end'>
-        <Link
-          className="w-100 mt-3 p-2"
+        <Link className="w-100 mt-3 p-2"
           to={`/product/${product.id}`}
-          state={{product: product, images: images}}
-        >
-          <button
-            className='w-100 mt-3 p-2'
-          >Quick view</button>
+          state={{ product: product, images: images }}>
+          <button className='w-100 mt-3 p-2'>
+            Quick view
+          </button>
         </Link>
       </div>
-      
+
     </div>
   );
 };
