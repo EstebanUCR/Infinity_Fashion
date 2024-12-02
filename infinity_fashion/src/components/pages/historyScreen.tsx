abdbad62dae510cafb91abf880aaadb3d12ecd7f
Esteban Iglesias Vargas <Esteban.Iglesias@ucr.ac.cr>
2024-12-01 21:48:38 -0600

    The page is created to show the purchase history

--- infinity_fashion/src/components/pages/historyScreen.tsx
+++ infinity_fashion/src/components/pages/historyScreen.tsx
@@ -1,39 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './historyScreen.css';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-import { CartItem, Product, ProductID } from '../../types/types';
+import { CartItem, Product, ProductID, userData } from '../../types/types';
+import { useNavigate } from 'react-router-dom';
+import useValidation from '../../hooks/useValidation';
+import { getPurchaseHistory } from '../../services/apiService';
+import HistoryScreen from '../historyScreen/HistoryScreen';
 
 type HistoryProps = {
-    cart: CartItem[]
-    removeFromCart: (id: ProductID) => void
-    increaseQuantity: (id: ProductID) => void
-    decreaseQuantity: (id: ProductID) => void
-    clearCart: () => void
-    isEmpty: boolean
-    cartTotal: number
-    data: Product[]
-    addToCart: (item: Product) => void
-  }
+  cart: CartItem[]
+  removeFromCart: (id: ProductID) => void
+  increaseQuantity: (id: ProductID) => void
+  decreaseQuantity: (id: ProductID) => void
+  clearCart: () => void
+  isEmpty: boolean
+  cartTotal: number
+  data: Product[]
+  addToCart: (item: Product) => void
+}
 
-export default function HistoryScreen ({ data, addToCart, cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal } : HistoryProps ){
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
 
-    return (
-        <div>
-        <Header 
-          cart={cart} 
-          removeFromCart={removeFromCart}
-          increaseQuantity={increaseQuantity}
-          decreaseQuantity={decreaseQuantity}
-          clearCart={clearCart}
-          isEmpty={isEmpty}
-          cartTotal={cartTotal}
-        />
-        <h1>Product not found</h1>
-        <Footer />
-      </div>
-    );
+
+export default function HistoryScreenComponent({ data, addToCart, cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal }: HistoryProps) {
+  const navigate = useNavigate();
+
+
+  const [messageModal, setMessageModal] = useState<{
+    isOpen: boolean;
+    message: string;
+    type: 'success' | 'error';
+  }>({
+    isOpen: false,
+    message: '',
+    type: 'success',
+  });
+
+
+
+  return (
+    <div>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+        isEmpty={isEmpty}
+        cartTotal={cartTotal}
+      />
+      <HistoryScreen />
+      <Footer />
+    </div>
+  );
 }
